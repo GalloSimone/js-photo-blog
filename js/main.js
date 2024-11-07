8cb77caa1f39aa9fd55f1a2d0d38f6a2860599f7
GalloSimone <gallosimonecz@gmail.com>
2024-11-07 16:27:43 +0100

    add image

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const cards = document.getElementById("post-card")
+const postNumber=6
+fetch("https://jsonplaceholder.typicode.com/photos?_limit="+ postNumber)
+.then((res)=>res.json())
+.then((posts) => {
+    console.log(posts);
+    
+    posts.forEach((post) => {
+      cards.innerHTML += `
+      
+        <div class="col">
+                <div class="card m-5 px-3>
+                  <img src="${post.thumbnailUrl}" class="card-img-top" alt="priima foto">
+                  <div class="card-body">
+                    <h5 class="card-title">${post.title}</h5>
+                   
+                  </div>
+                </div>
+              </div>
+
+            
+      `
+    });
+})
