19a64cf1f60046d64feec3743e9466131458529c
GalloSimone <gallosimonecz@gmail.com>
2024-11-08 15:52:35 +0100

    add overlay

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
 const cards = document.getElementById("post-card")
-const postNumber=6
-fetch("https://jsonplaceholder.typicode.com/photos?_limit="+ postNumber)
+const layover = document.getElementById("leyover")
+const photoNumber=6
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
 .then((res)=>res.json())
-.then((posts) => {
-    console.log(posts);
-    
-    posts.forEach((post) => {
-      cards.innerHTML += `
-      
-        <div class="col">
-                <div class="card m-5 px-3>
-                  <img src="${post.Url}" class="card-img-top" alt="priima foto">
+.then((photos)=>{
+  console.log(photos);
+
+  photos.forEach((photo)=>{
+    cards.innerHTML +=`
+     <div class="col">
+                <div class="card m-5 px-3 >
+                  <img src="${photo.url}" class="card-img-top" alt="priima foto">
                   <div class="card-body">
-                    <h5 class="card-title">${post.title}</h5>
+                    <h5 class="card-title">${photo.title}</h5>
                    
                   </div>
                 </div>
               </div>
 
-            
-      `
-    });
-})
+    `
+  }
+
+  )
+  
+}
+
+)
